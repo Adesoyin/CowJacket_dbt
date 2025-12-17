4d6573440c3ec4e8888815f615e2ac73a3d561a1
Adebola Adesoyin <adeboladesoyin@gmail.com>
2025-12-17 18:31:46 +0000

    changed loaded date

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,5 +4,6 @@ order_id,
 customer_id, 
 order_date, 
 total_amount,
-CURRENT_TIMESTAMP() as etl_loaded_at
+--CURRENT_TIMESTAMP() as etl_loaded_at
+{{ dbt.dateadd(datepart='hour', interval=-3, from_date_or_timestamp='current_timestamp()') }} as etl_loaded_at
 from {{ source ('dec_raw', 'orders') }}
